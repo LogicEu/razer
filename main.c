ee5e6c3a55fe015f247464ef3fa5e938b8baee8e
LogicEu <eugenioarteagaa@gmail.com>
2021-08-25 19:05:15 -0400

    added multisampling to smooth edges

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+uint8_t ulerp(uint8_t a, uint8_t b, float t)
+{
+    return (int)lerpf((float)a, (float)b, t);
+}
+
 int main(int argc, char** argv)
 {
     uint8_t r, g, b;
@@ -17,7 +22,7 @@ int main(int argc, char** argv)
 
     char path[256] = "output.png", open[256];
     ivec2 res = ivec2_new(800, 600);
-    int channels = 4;
+    int channels = 4, samples = 4;
 
     bmp_t bmp = bmp_color(res.x, res.y, channels, &color[0]);
     Circle c = circle_new(vec2_new(res.x / 2, res.y / 2), 10.0f);
@@ -30,8 +35,21 @@ int main(int argc, char** argv)
     for (unsigned int y = 0; y < bmp.height; y++) {
         for (unsigned int x = 0; x < bmp.width; x++) {
             vec2 p = {x, y};
-            if (tri2D_point_overlap(&t, p)) memcpy(px_at(&bmp, x, y), &blue[0], bmp.channels);
-            if (circle_point_overlap(&c, p)) memcpy(px_at(&bmp, x, y), &red[0], bmp.channels);
+            float unit = 1.0 / (float)samples;
+            int tricheck = 0, circheck = 0;
+            for (unsigned int yy = 0; yy < samples; yy++) {
+                for (unsigned int xx = 0; xx < samples; xx++) {
+                    vec2 pp = {p.x + (float)xx * unit, p.y + (float)yy * unit};
+                    if (tri2D_point_overlap(&t, pp)) tricheck++;
+                    if (circle_point_overlap(&c, pp)) circheck++;
+                }
+            }
+            unit /= (float)samples;
+            uint8_t* px = px_at(&bmp, x, y);
+            for (int i = 0; i < 4; i++) {
+                px[i] = ulerp(px[i], blue[i], (float)tricheck * unit);
+                px[i] = ulerp(px[i], red[i], (float)circheck * unit);
+            }
         }
     }
     
